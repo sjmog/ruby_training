95c253ab30c4240bc500062902cbeb17b3b9a3bc
David Morgan <davidmorgan@Davids-MacBook-Pro-2.local>
2019-08-17 17:26:24 +0100

    'Subtracting with nothing' complete

---
+++ 1/calculator.rb
@@ -0,0 +1,21 @@
+#Constants defined
+
+ONE = 1
+TWO = 2
+THREE = 3
+FOUR = 4
+FIVE = 5
+SIX = 6
+SEVEN = 7
+EIGHT = 8
+NINE = 9
+
+#Functions defined
+
+def add(number=0,number2=0, number3=0,number4=0)
+	number + number2 + number3 + number4
+end
+
+def subtract
+	0
+end
