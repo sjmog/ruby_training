0b57a3515cc7e8380be8089f190f54718c404d4f
David Morgan <d.h.morgan@hotmail.co.uk>
2019-08-18 19:51:26 +0100

    Added student_loan & national_insurance functions

--- 1/tax_calculator.rb
+++ 1/tax_calculator.rb
@@ -2,6 +2,18 @@
 #1. type 'irb' into the terminal
 #2. type 'require "./tax_calculator"' and hit Enter
 
+#constants defined
+
+
+#functions defined
 def income_tax(income=0)
   income/10
+end
+
+def student_loan(income=0)
+  income/20
+end
+
+def national_insurance(income=0)
+  income/20
 end
