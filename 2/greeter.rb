09c868db7301e7f3c4ce2e7971a6d129294b94be
David Morgan <d.h.morgan@hotmail.co.uk>
2019-08-18 19:10:56 +0100

    Removed 'do' and 'end' commands. Removing duplicate 'calculator.rb' file

--- 2/greeter.rb
+++ 2/greeter.rb
@@ -1,10 +1,9 @@
-print 'Please enter your name:' do
-end
+print 'Please enter your name: '
 
 name=gets.chomp
 
 if name==''
-	return puts 'Hello, PERSON WITH NO NAME!'
+  return puts 'Hello, PERSON WITH NO NAME!'
 else
-	return puts 'Hello, ' + name.upcase + "!"  
+  return puts 'Hello, ' + name.upcase + "!"  
 end
