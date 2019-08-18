6f7e7e49bdac71fc350371de195681cbe8022ec5
David Morgan <d.h.morgan@hotmail.co.uk>
2019-08-18 20:30:43 +0100

    Take home pay calculated w/ variable income tax, student loans and national insurance

--- 1/tax_calculator.rb
+++ 1/tax_calculator.rb
@@ -6,14 +6,18 @@
 
 
 #functions defined
-def income_tax(income=0)
-  income/10
+def income_tax(income=0,inc_tax_rate=0.1)
+  income*inc_tax_rate
 end
 
-def student_loan(income=0)
-  income/20
+def student_loan(income=0,stu_loan_rate=0.05)
+  income*stu_loan_rate
 end
 
-def national_insurance(income=0)
-  income/20
+def national_insurance(income=0,nat_ins_rate=0.05)
+  income*nat_ins_rate
+end
+
+def take_home_pay(income=0,inc_tax_rate=0.1,stu_loan_rate=0.05,nat_ins_rate=0.05)
+  income*(1-(inc_tax_rate+stu_loan_rate+nat_ins_rate))
 end
