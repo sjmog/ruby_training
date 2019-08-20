08ddc4ce5322c88a25c1e08ede81072682974fae
David Morgan <d.h.morgan@hotmail.co.uk>
2019-08-20 23:10:51 +0100

    Started Fizzbuzz exercise, defined greeter setting a range of an array  Please enter the commit message for your changes. Lines starting

---
+++ 3/fizzbuzz.rb
@@ -0,0 +1,14 @@
+#Constants defined
+FIZZ = 3
+
+
+#Functions defined
+puts "What should the start number be?"
+
+start_number = gets.chomp
+
+puts "And what should the end number be?"
+
+end_number = gets.chomp
+
+puts (start_number..end_number).to_a
