3e74a2fff19380790267255c86d6361d8f5ac755
David Morgan <d.h.morgan@hotmail.co.uk>
2019-09-02 18:03:34 +0100

    Requests 1 & 2 complete & code tidied up

--- 3/tax_calculator3.rb
+++ 3/tax_calculator3.rb
@@ -11,26 +11,18 @@ def national_insurance(income=0,nat_ins_rate=0.05,base_tax_rate=0.1,low_tax_rate
  net_pay(income,base_tax_rate,low_tax_rate,med_tax_rate,high_tax_rate)*nat_ins_rate
 end
 
-#def income_tax(income=0,inc_tax_rate=0.1)
-#  income*inc_tax_rate
-#end
-
-#def take_home_pay(income=0,inc_tax_rate=0.1,stu_loan_rate=0.05,nat_ins_rate=0.05)
-#  income - income_tax(income,inc_tax_rate) - student_loan(income,stu_loan_rate) - national_insurance(income,nat_ins_rate)
-#end
-
 #Population formulae
 
 def government_revenue_per_head(income=0,base_tax_rate=0,low_tax_rate=0.2,med_tax_rate=0.4,high_tax_rate=0.5,stu_loan_rate=0.05,nat_ins_rate=0.05)
   income - take_home_pay(income,base_tax_rate,low_tax_rate,med_tax_rate,high_tax_rate,stu_loan_rate,nat_ins_rate)
 end
 
-def fraction_of_people_who_will_pay(inc_tax_rate=0.1,stu_loan_rate=0.05,nat_ins_rate=0.05)
-  (1-(inc_tax_rate+stu_loan_rate+nat_ins_rate)).round(2)
+def fraction_of_people_who_will_pay(base_tax_rate=0,low_tax_rate=0.2,med_tax_rate=0.4,high_tax_rate=0.5,stu_loan_rate=0.05,nat_ins_rate=0.05)
+  (1-(base_tax_rate+ 0.5*low_tax_rate+0.3*med_tax_rate+0.1*high_tax_rate+stu_loan_rate+nat_ins_rate)).round(2)
 end
 
-def government_revenue(av_income=0,inc_tax_rate=0.1,stu_loan_rate=0.05,nat_ins_rate=0.05,pop_size=1000000)
-  (pop_size*fraction_of_people_who_will_pay(inc_tax_rate,stu_loan_rate,nat_ins_rate)*government_revenue_per_head(av_income,inc_tax_rate,stu_loan_rate,nat_ins_rate)).round(0)
+def government_revenue(income=0,base_tax_rate=0,low_tax_rate=0.2,med_tax_rate=0.4,high_tax_rate=0.5,stu_loan_rate=0.05,nat_ins_rate=0.05,pop_size=1000000)
+  (pop_size*fraction_of_people_who_will_pay(base_tax_rate,low_tax_rate,med_tax_rate,high_tax_rate,stu_loan_rate,nat_ins_rate)*government_revenue_per_head(income,base_tax_rate,low_tax_rate,med_tax_rate,high_tax_rate,stu_loan_rate,nat_ins_rate)).round(0)
 end
 
 #Aggregate taxation formulae
