a77c69dbbfe9ce814cb77b9f2cf84ebc76de89ed
David Morgan <d.h.morgan@hotmail.co.uk>
2019-09-02 17:51:06 +0100

    'take_home_pay' & 'government_revenue_per_head' functions added & enhanced - non-income taxes deducted from net income (after income tax)

--- 3/tax_calculator3.rb
+++ 3/tax_calculator3.rb
@@ -3,41 +3,66 @@
 
 #Calculator
 
-def student_loan(income=0,stu_loan_rate=0.05)
-  income*stu_loan_rate
+def student_loan(income=0,stu_loan_rate=0.05,base_tax_rate=0.1,low_tax_rate=0.2,med_tax_rate=0.4,high_tax_rate=0.5)
+  net_pay(income,base_tax_rate,low_tax_rate,med_tax_rate,high_tax_rate)*stu_loan_rate
 end
 
-def national_insurance(income=0,nat_ins_rate=0.05)
-  income*nat_ins_rate
+def national_insurance(income=0,nat_ins_rate=0.05,base_tax_rate=0.1,low_tax_rate=0.2,med_tax_rate=0.4,high_tax_rate=0.5)
+ net_pay(income,base_tax_rate,low_tax_rate,med_tax_rate,high_tax_rate)*nat_ins_rate
 end
 
+#def income_tax(income=0,inc_tax_rate=0.1)
+#  income*inc_tax_rate
+#end
 
+#def take_home_pay(income=0,inc_tax_rate=0.1,stu_loan_rate=0.05,nat_ins_rate=0.05)
+#  income - income_tax(income,inc_tax_rate) - student_loan(income,stu_loan_rate) - national_insurance(income,nat_ins_rate)
+#end
 
-def government_revenue_per_head(income=0, inc_tax_rate=0.1,stu_loan_rate=0.05,nat_ins_rate=0.05)
-  income - take_home_pay(income,inc_tax_rate,stu_loan_rate,nat_ins_rate)
+#Population formulae
+
+def government_revenue_per_head(income=0,base_tax_rate=0,low_tax_rate=0.2,med_tax_rate=0.4,high_tax_rate=0.5,stu_loan_rate=0.05,nat_ins_rate=0.05)
+  income - take_home_pay(income,base_tax_rate,low_tax_rate,med_tax_rate,high_tax_rate,stu_loan_rate,nat_ins_rate)
 end
 
 def fraction_of_people_who_will_pay(inc_tax_rate=0.1,stu_loan_rate=0.05,nat_ins_rate=0.05)
-  (1-(inc_tax_rate+stu_loan_rate+nat_ins_rate)).round(1)
+  (1-(inc_tax_rate+stu_loan_rate+nat_ins_rate)).round(2)
 end
 
-def government_revenue(av_income=0, inc_tax_rate=0.1, stu_loan_rate=0.05, nat_ins_rate=0.05,pop_size=1000000)
+def government_revenue(av_income=0,inc_tax_rate=0.1,stu_loan_rate=0.05,nat_ins_rate=0.05,pop_size=1000000)
   (pop_size*fraction_of_people_who_will_pay(inc_tax_rate,stu_loan_rate,nat_ins_rate)*government_revenue_per_head(av_income,inc_tax_rate,stu_loan_rate,nat_ins_rate)).round(0)
 end
 
-######### Redefined formulae to account for income tax bands. Other taxes ignored #########
+#Aggregate taxation formulae
 
-def take_home_pay(income=0,high_tax_rate=0.5,med_tax_rate=0.4,low_tax_rate=0.2)
-	income - high_tax(income,high_tax_rate) - med_tax(income,med_tax_rate) - low_tax(income,low_tax_rate)
+def income_tax(income=0,base_tax_rate=0,low_tax_rate=0.2,med_tax_rate=0.4,high_tax_rate=0.5)
+base_tax(income,base_tax_rate) + low_tax(income,low_tax_rate) + med_tax(income,med_tax_rate) + high_tax(income,high_tax_rate)
 end
 
-def income_tax(income=0,low_tax_rate=0.2,med_tax_rate=0.4,high_tax_rate=0.5)
-low_tax(income,low_tax_rate) + med_tax(income,med_tax_rate) + high_tax(income,high_tax_rate)
+def net_pay(income=0,base_tax_rate=0,low_tax_rate=0.2,med_tax_rate=0.4,high_tax_rate=0.5)
+  income - base_tax(income,base_tax_rate) - low_tax(income,low_tax_rate) - med_tax(income,med_tax_rate) - high_tax(income,high_tax_rate)
 end
 
-def high_tax(income=0,high_tax_rate=0.5)
-	if income > 100000
-		(income - 100000)*high_tax_rate
+def take_home_pay(income=0,base_tax_rate=0,low_tax_rate=0.2,med_tax_rate=0.4,high_tax_rate=0.5,stu_loan_rate=0.05,nat_ins_rate=0.05)
+  net_pay(income,base_tax_rate,low_tax_rate,med_tax_rate,high_tax_rate) - student_loan(income,stu_loan_rate,base_tax_rate,low_tax_rate,med_tax_rate,high_tax_rate) - national_insurance(income,nat_ins_rate,base_tax_rate,low_tax_rate,med_tax_rate,high_tax_rate)
+end
+
+
+#Tax bands defined for income
+
+def base_tax(income=0,base_tax_rate=0)
+	if income >=0 && income <=20000
+		income*base_tax_rate
+	elsif income > 20000
+		20000*base_tax_rate
+	 else 0
+  end
+end
+def low_tax(income=0,low_tax_rate=0.2)
+	if income > 20000 && income <= 40000
+		(income - 20000)*low_tax_rate
+	elsif income > 40000
+		20000*low_tax_rate
 	else 0
   end
 end
@@ -51,25 +76,21 @@ def med_tax(income=0,med_tax_rate=0.4)
   end
 end
 
-def low_tax(income=0,low_tax_rate=0.2)
-	if income > 20000 && income <= 40000
-		(income - 20000)*low_tax_rate
-	elsif income > 40000
-		20000*low_tax_rate
+def high_tax(income=0,high_tax_rate=0.5)
+	if income > 100000
+		(income - 100000)*high_tax_rate
 	else 0
   end
 end
 
 #Greeter
 
-puts "Greetings, citizen! What is your name?"
+#puts "Greetings, citizen! What is your name?"
 
-name = gets.chomp
+#name = gets.chomp
 
-puts "Ok " + name + " my name is Tax Calculator. I'm here to calculate your take-home pay. What is your income?"
+#puts "Ok " + name + " my name is Tax Calculator. I'm here to calculate your take-home pay. What is your income?"
 
-income = gets.chomp
+#income = gets.chomp
 
-
-
-puts "I see your income is " + income + ". For that income, your take-home pay would be " + take_home_pay(income.to_i).to_s + "."
+#puts "I see your income is " + income + ". For that income, your take-home pay would be " + take_home_pay(income.to_i).to_s + "."
